87bbdd4623aa2e391c02b6279949c81b2931b7d9
Aryan Jha <aryanjha026@gmail.com>
2022-10-29 00:39:30 +0530

    Added User

--- backend/user.py
+++ backend/user.py
@@ -1,4 +1,10 @@
 class User:
     def __init__(self) -> None:
         self.balance = 0
-    
+        self.qty = dict()
+
+    def add_balance(self, x: int):
+        self.balance += x
+
+    def change_qty(self, stock_name: str, x: int):
+        self.qty[stock_name] += x
