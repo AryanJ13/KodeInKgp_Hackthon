a63fcfc2671d14f65c645dbd165a5e626fe319c9
wszdexdrf <piyushkumar2k02@kgpian.iitkgp.ac.in>
2022-10-29 14:56:17 +0530

    Modify graph

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,58 @@
 import React from "react";
+import {
+  AreaChart,
+  ResponsiveContainer,
+  Legend,
+  Tooltip,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+} from "recharts";
 import "./App.css";
 import OrderBook from "./OrderBook";
 
+var a = [...Array(50).keys()];
+var b = Array.from({ length: 50 }, () => Math.floor(Math.random() * 50));
+b.sort();
+
+const pdata = a.map(function (e, i) {
+  return { index: e, value: b[i] };
+});
+
 const data = [
-    {order_id: 1, seller: "A", buyer: "D", qunatity: 1, price: 10},
-    {order_id: 2, seller: "B", buyer: "E", qunatity: 2, price: 30},
-    {order_id: 3, seller: "C", buyer: "F", qunatity: 3, price: 20},
-  ]
-    
-  function App() {
-    return (
-      <div className="App">
+  { order_id: 1, seller: "A", buyer: "D", qunatity: 1, price: 10 },
+  { order_id: 2, seller: "B", buyer: "E", qunatity: 2, price: 30 },
+  { order_id: 3, seller: "C", buyer: "F", qunatity: 3, price: 20 },
+  { order_id: 4, seller: "Q", buyer: "E", qunatity: 4, price: 40 },
+  { order_id: 5, seller: "W", buyer: "R", qunatity: 2, price: 50 },
+];
+
+function App() {
+  return (
+    <div className="Kodein">
+      <ResponsiveContainer width="80%" aspect={3}>
+        <AreaChart data={pdata} margin={{ right: 300 }}>
+          <defs>
+            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="index" interval={"preserveStartEnd"} />
+          <YAxis></YAxis>
+          <Legend />
+          <Tooltip />
+          <Area
+            dataKey="value"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#colorUv)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+      <div class = "tradebook">
         <table>
           <tr>
             <th>Order Id</th>
@@ -28,18 +70,14 @@ const data = [
                 <td>{val.qunatity}</td>
                 <td>{val.price}</td>
               </tr>
-            )
+            );
           })}
         </table>
-        <div>
-        <h2>Crypto Order Book </h2>
-        <OrderBook />
-        </div>
-
       </div>
-    );
-  }
-
-
-
+      <div>
+        <OrderBook />
+      </div>
+    </div>
+  );
+}
 export default App;
